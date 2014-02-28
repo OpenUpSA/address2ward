af5d48bfca182c8585599502ea8297844de9a99e
Adi Eyal <adi@burgercom.co.za>
2014-02-28 10:37:21 +0200

    Refactored geocoding code - added esri wsg as a fallback
    ESRI is very slow and not as good unfortunately but it doesn't have limits. Google only allows for 2,500 resolutions per day

--- convert.py
+++ convert.py
@@ -4,6 +4,11 @@ from geopy.geocoders import GoogleV3
 import config
 import psycopg2
 from datetime import datetime
+from omgeo import Geocoder
+import urllib2, urllib
+import logging
+
+logger = logging.getLogger(config.LOGGER_NAME)
 
 default_db = "wards_2006"
 class AddressConverter(object):
@@ -11,13 +16,50 @@ class AddressConverter(object):
         self.curs = curs
         self.geolocator = GoogleV3()
         
+        self.geocoder = Geocoder()
+        
+    def resolve_address_google(self, address):
+        try:
+            address = urllib.quote(address)
+            url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false&region=za&key=%s" % (address, config.google_key)
+            response = urllib2.urlopen(url)
+            js = response.read()
+            try:
+                js = json.loads(js)
+            except ValueError:
+                logger.exception("Error trying to resolve %s" % address)
+                return None
+
+            if "results" in js and len(js["results"]) > 0:
+                result = js["results"][0]
+                geom = js["results"][0]["geometry"]["location"]
+                lat = geom["lat"]; lng = geom["lng"]   
+                formatted_address = result["formatted_address"]
+
+                return formatted_address, lat, lng
+        except Exception:
+            logger.exception("Error trying to resolve %s" % address)
+        return None
+        
+    def resolve_address_esri(self, address):
+        result = self.geocoder.geocode(address + ", South Africa")
+        if not result: return None
+        if not "candidates" in result: return None
+        if len(result["candidates"]) == 0: return None
+        candidate = result["candidates"][0]
+        address = candidate.match_addr
+        latitude = candidate.y
+        longitude = candidate.x
+
+        return address, latitude, longitude
+        
     def convert(self, address):
         now1 = datetime.now()
-        result = self.geolocator.geocode(address, region="za")
-        now2 = datetime.now()
+        result = self.resolve_address_google(address)
         if not result: return None
+        address, latitude, longitude = result
+        now2 = datetime.now()
 
-        address, (latitude, longitude) = result
         poi = (longitude, latitude)
         sql = """
             SELECT
@@ -48,8 +90,8 @@ class AddressConverter(object):
 
 if __name__ == "__main__":
     conn = psycopg2.connect(
-        database=config.database, user="wards",
-        host="localhost", password="wards"
+        database=config.database, user=config.db_user,
+        host=config.db_host, password=config.db_password
     )
     try:
         curs = conn.cursor()
