84a085cbda9a2d43817ae5262c8559fad8aeb439
Adi Eyal <adi@burgercom.co.za>
2014-04-09 11:35:12 +0200

    Fixed fabfile bugs

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
 from fabric.operations import local, run
 from fabric import api
 import os
-from config import configure
+from config import configuration
 
 api.env.hosts = ["adi@code4sa.org:2222"]
 
 def setup():
-    db_config = configure["databases"]["wards_2006"]
+    db_config = configuration["databases"]["wards_2006"]
     local("pip install -r requirements/base.txt") 
     if not os.path.exists("{database}.sql".format(database=db_config["database"])):
         local("wget http://wards.code4sa.org/static/sql/{database}.sql".format(database=db_config["database"]))
@@ -23,9 +23,9 @@ def run_web():
     local("python web.py")
 
 def deploy():
-    with api.cd(configure["environment"]["code_dir"]):
+    with api.cd(configuration["environment"]["code_dir"]):
         api.run("git pull origin master")
-        api.run("%s install -r %s/requirements/web.txt --quiet" % (configure["environment"]["pip"], config["environment"]["code_dir"]))
+        api.run("%s install -r %s/requirements/web.txt --quiet" % (configuration["environment"]["pip"], configuration["environment"]["code_dir"]))
 
         api.sudo("supervisorctl restart wards")
 
